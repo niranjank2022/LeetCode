1d787e9bfeaecbf501f4e2ee5564f9b732fe901c
Niranjan <niranjank.desktop@gmail.com>
2023-05-16 16:24:09 +0530

    Update file

--- Easy/26. Remove Duplicates from Sorted Array.py
+++ Easy/26. Remove Duplicates from Sorted Array.py
@@ -1,16 +1,22 @@
 def removeDuplicates(nums: list[int]) -> int:
-    i = len(nums) - 1
-    while i > 0:
-        if nums[i] == nums[i - 1]:
-            nums.pop(i)
-        i -= 1
+    n = len(nums)
+    i, j = 1, 1
+    while j < n:
+        while j < n and nums[j] == nums[j - 1]:
+            j += 1
+        if j < n:
+            nums[i] = nums[j]
+            i += 1
+            j += 1
+
+    return i
 
 
 def main():
     testcases = [[-1, 0, 0, 0, 0], [1, 1, 2], [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]]
     for case in testcases:
         print(case, end='\t')
-        removeDuplicates(case)
+        print(removeDuplicates(case))
         print(case)
 
 
--- Easy/27. Remove Element.py
+++ Easy/27. Remove Element.py
@@ -1,11 +1,23 @@
 def removeElement(nums: list[int], val: int) -> int:
-    for i in range(nums.count(val)):
-        nums.remove(val)
+    n = len(nums)
+    i = j = 0
+    while i < n and j < n:
+        while i < n and nums[i] != val:
+            i += 1
+        while j < n and nums[j] == val:
+            j += 1
 
-    return len(nums)
+        if i < j < n:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+        j += 1
+
+    return i
 
 
 if __name__ == '__main__':
     testcases = [([0, 1, 2, 2, 3, 0, 4, 2], 2), ([3, 2, 2, 3], 3)]
     for case in testcases:
-        print(f"{case} -> {removeElement(case[0], case[1])} {case[0]}")
+        print(case, end='\t')
+        removeElement(case[0], case[1])
+        print(case[0])
