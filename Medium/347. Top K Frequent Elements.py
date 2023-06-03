886b8184eb2f9d55ebf3b4cc8b8a1a0dd9f890c3
Niranjan <niranjank.desktop@gmail.com>
2023-06-03 19:35:02 +0530

    Update file

--- Medium/347. Top K Frequent Elements.py
+++ Medium/347. Top K Frequent Elements.py
@@ -1,19 +1,27 @@
+import collections
 import heapq
 
 
 def topKFrequent(nums: list[int], k: int) -> list[int]:
     frequent = {}
-    answer = []
     for num in nums:
         frequent[num] = frequent.get(num, 0) + 1
 
     pairs = sorted(list(frequent.items()), key=lambda x: x[1], reverse=True)
-    for i in range(k):
-        answer.append(pairs[i][0])
-    return answer
+    return [pairs[i][0] for i in range(k)]
+
+    """
+    count = collections.Counter(nums)
+    heap = [key for key in count]
+    print("***", heap)
+    heapq.heapify(heap)
+
+    print(heap, "***")
+    return [heapq.heappop(heap) for _ in range(k)]
+    """
 
 
 if __name__ == '__main__':
-    testcases = [([1, 1, 1, 2, 2, 3], 2), ([1], 1)]
+    testcases = [([1, 1, 1, 2, 2, 3], 2), ([1], 1), ([4, 1, -1, 2, -1, 2, 3], 2)]
     for case in testcases:
         print(f"{case} -> {topKFrequent(case[0], case[1])}")
