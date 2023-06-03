1a3b200ae350c0692a0642abcacae83380923496
Niranjan <niranjank.desktop@gmail.com>
2023-06-03 20:13:01 +0530

    Revert "Update file"
    This reverts commit f6f7a2cead93edc9aa8a1b29f29e4e738cce1f29.

--- Easy/599. Minimum Index Sum of Two Lists.py
+++ Easy/599. Minimum Index Sum of Two Lists.py
@@ -19,12 +19,3 @@ def findRestaurant(list1: list[str], list2: list[str]) -> list[str]:
             minSum = min(minSum, indexSum)
 
     return answer
-
-
-if __name__ == '__main__':
-    testcases = [[["Shogun", "Tapioca Express", "Burger King", "KFC"], ["KFC", "Shogun", "Burger King"]],
-                 [["happy", "sad", "good"], ["sad", "happy", "good"]],
-                 [["Shogun", "Tapioca Express", "Burger King", "KFC"], ["KFC", "Shogun", "Burger King"]]]
-
-    for case in testcases:
-        print(f"{case} -> {findRestaurant(case[0], case[1])}")
